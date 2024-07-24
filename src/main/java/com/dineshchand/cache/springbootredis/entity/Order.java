9774d6a37fdc8f003b4f8ad8c827e0db26f7f954
finvedicdev <128971579+finvedicdev@users.noreply.github.com>
2024-07-24 16:25:51 +0530

    serial number fix

--- src/main/java/com/dineshchand/cache/springbootredis/entity/Order.java
+++ src/main/java/com/dineshchand/cache/springbootredis/entity/Order.java
@@ -19,7 +19,6 @@ import java.time.Instant;
 @Table(name = "order_details")
 public class Order implements Serializable{
 
-    private static final long serialVersionUID = 1L;
 
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
